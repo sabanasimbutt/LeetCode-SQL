bab0cd815d9c7237570d7b31be0c8464079dbd07
saba.nasim <saba.nasim@lovefordata.com>
2022-10-02 21:30:11 +0500

    Updated

--- SQL/177. Nth Highest Salary.sql
+++ SQL/177. Nth Highest Salary.sql
@@ -12,6 +12,7 @@
 
 # Write an SQL query to report the nth highest salary from the Employee table. If there is no nth highest salary, the query should report null.
 
+
 # The query result format is in the following example.
 
 #------------------------------------------------------------------------------
